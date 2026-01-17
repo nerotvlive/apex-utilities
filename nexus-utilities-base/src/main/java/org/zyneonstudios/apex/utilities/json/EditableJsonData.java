65366a74ec58be9e9ad30732127f66aaf3d39437
nerotvlive <daniel.niesmann@outlook.com>
2026-01-17 20:32:14 +0100

    Update project structure

--- nexus-utilities-base/src/main/java/org/zyneonstudios/apex/utilities/json/EditableJsonData.java
+++
@@ -1,8 +0,0 @@
-package org.zyneonstudios.apex.utilities.json;
-
-import org.zyneonstudios.apex.utilities.storage.EditableStorage;
-
-public interface EditableJsonData extends JsonData, EditableStorage {
-
-    void clear();
-}
